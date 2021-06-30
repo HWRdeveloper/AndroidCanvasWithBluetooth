2c9175e538ccf2f948aca492d8a0fc857f03774a
HWRdeveloper <ruehyeon12@gmail.com>
2021-06-30 16:27:54 +0900

    commit

---
+++ app/src/main/java/com/droiduino/bluetoothconn/draw/widget/Action.kt
@@ -0,0 +1,12 @@
+package com.droiduino.bluetoothconn.draw.widget
+
+
+import android.graphics.Path
+import java.io.Serializable
+import java.io.Writer
+
+interface Action : Serializable {
+    fun perform(path: Path)
+
+    fun perform(writer: Writer)
+}
